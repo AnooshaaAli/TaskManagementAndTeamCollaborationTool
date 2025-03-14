7cd755a38523c1f49d1987f15dfd7aadccae9da7
Saleha Irum <117927283+salehairum@users.noreply.github.com>
2025-03-14 11:38:39 +0500

    linking project api with list api for get/post+AppConfig.java

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import RegisterPage from "./pages/RegisterPage";
 import './App.css';
+import List from "./components/List.jsx"
 
 function App() {
   return (
     <div>
       <RegisterPage />
+      {/* saleha testing stuff
+      <List name="To Do" />
+      <List name="Done" /> */}
     </div>
   );
 }
---
+++ frontend/src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const List = ({ name }) => {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <div>
+                blah blah
+            </div>
+        </div>
+    );
+};
+
+export default List;
