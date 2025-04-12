b9e2f1b841ae3fb7062421c5898dc9c2ff35c2fd
Saleha Irum <117927283+salehairum@users.noreply.github.com>
2025-04-12 22:59:51 +0500

    basic comment files, frontend and backend

---
+++ backend/src/main/java/com/example/demo/Services/CommentService.java
@@ -0,0 +1,44 @@
+package com.example.demo.Services;
+
+import com.example.demo.Models.Comment;
+import com.example.demo.Repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public Comment createComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public void deleteComment(int id) {
+        commentRepository.deleteById(id);
+    }
+
+    public List<Comment> getAllComments() {
+        return commentRepository.findAll();
+    }
+
+    public Optional<Comment> getCommentById(int id) {
+        return commentRepository.findById(id);
+    }
+
+    public List<Comment> getCommentsByProjectId(int projectId) {
+        return commentRepository.findByProjectID(projectId);
+    }
+
+    public List<Comment> getCommentsByUserId(int userId) {
+        return commentRepository.findByUserID(userId);
+    }
+}
